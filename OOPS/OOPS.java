b23fa4b5ee2089c8ffbb8f8a0d4c8b3695576caf
Pratik <edu.pratik21@gmail.com>
2026-06-25 05:55:35 +0530

    update polymorphism code

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -1,4 +1,3 @@
-//--------------Blueprint of Pen-------------------------------------
 //classes name stert with capital letters and functions name start with small letter
 class Pen{
     String color;
@@ -14,22 +13,61 @@ class Pen{
 }
 
 class Student {
+    String name;
+    int age;
 
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    Student(){ //non-parametrised constructor
+        System.out.println("Constructor called");
+    }
+
+    Student(String name, int age){ //non-parametrised constructor
+        this.name = name;
+        this.age = age;
+    }
+
+    //Copy constructor
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
 }
 
 
 public class OOPS {
     public static void main(String[] args) {
-        Pen pen1 = new Pen();  //creating object of pens
-        pen1.color = "Blue";
-        pen1.type = "Gel";
-        pen1.write();
-        pen1.printColor();
+        // Pen pen1 = new Pen();  //creating object of pens
+        // pen1.color = "Blue";
+        // pen1.type = "Gel";
+        // pen1.write();
+        // pen1.printColor();
 
-        Pen pen2 = new Pen();
-        pen2.color = "Black";
-        pen2.type = "Ballpoint";
-        pen2.write();
-        pen2.printColor();
+        // Pen pen2 = new Pen();
+        // pen2.color = "Black";
+        // pen2.type = "Ballpoint";
+        // pen2.write();
+        // pen2.printColor();
+        
+
+        //Constructor --> class name and constructor name should be same
+                 //   --> COnstructor will not return anything
+        // Student s1 = new Student();   //Object creation
+        // s1.name = "Atharva"; 
+        // s1.age = 17;
+        // s1.printInfo();
+
+        // Parameterised Constructor
+        // Student s2 = new Student("Pratik",25);   //Object creation
+        // s2.printInfo();
+
+        // Copy constructor
+        Student s1 = new Student();
+        s1.name = "Ibin";
+        s1.age = 25;
+        Student s2 = new Student(s1);
+        s2.printInfo();
     }
 }
